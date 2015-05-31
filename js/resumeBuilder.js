2ab3eec9be66cc0adb00ca0171e6da5ad332304c
kchao-git <kchao394@gmail.com>
2015-05-30 17:19:28 -0700

    Add bio info

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,5 +1,45 @@
-var formattedName = HTMLheaderName.replace("%data%", "Kenny Chao");
-var formattedRole = HTMLheaderRole.replace("%data%", "Web Developer");
+//Bio Info
+var bio = {
+	name: 'Kenny Chao',
+	role: 'Web Developer',
+	contacts: {
+		email: 'kchao394@gmail.com',
+		github: 'kchao-git',
+		location: 'Santa Ana'
+	},
+	photo: 'images/fry.jpg',
+	message: 'Hello there!',
+	skills: ['HTML', 'CSS', 'Javascript']
+};
 
-$("#header").prepend(formattedRole);
-$("#header").prepend(formattedName);
+//Name and Role Header
+var formattedName = HTMLheaderName.replace('%data%', bio.name);
+var formattedRole = HTMLheaderRole.replace('%data%', bio.role);
+
+//Contact Info
+var formattedContact = '<ul>' + HTMLemail.replace('%data%', bio.contacts.email) + 
+	HTMLgithub.replace('%data%', bio.contacts.github) +
+	HTMLlocation.replace('%data%', bio.contacts.location) +
+	'</ul>';
+
+//Profile Pic
+var formattedPic = HTMLbioPic.replace('%data%', bio.photo);
+
+//Welcome Message
+var formattedWelcome = HTMLwelcomeMsg.replace('%data%', bio.message);
+
+//Skills
+var formattedSkills = '<ul>';
+bio.skills.forEach(function(skill) {
+	formattedSkills += HTMLskills.replace('%data%', skill);
+});
+formattedSkills += '</ul>';
+
+$('#header').prepend(formattedSkills);
+$('#header').prepend(HTMLskillsStart);
+$('#header').prepend(formattedWelcome);
+$('#header').prepend(formattedPic);
+$('#header').prepend(formattedContact);
+$('#header').prepend(formattedRole);
+$('#header').prepend(formattedName);
+
